7a301ce4fee461e0de84cd3e38351a1a5dfb9064
tmtenbrink <T.M.tenBrink@student.tudelft.nl>
2020-04-28 13:41:36 +0200

    nieuwe specifieke methode

--- model/specific_algorithm.py
+++ model/specific_algorithm.py
@@ -77,16 +77,73 @@ def cycle_sequence_assignment():
 
 def min_slot_assignment():
     for i in range(surgery_amount):
-        surgery = surgeries[i, 0]
+        # make it into list to make it
+        surgery = surgeries[i, :]
+        surgery_id = surgery[0]
 
         # k is next operation room to add slot to, it is the one with the least current time
-        k = np.argmin(slot_sums)
-        schedule[k].append(surgery)
-        slot_sums[k] += df_surg.iloc[surgery].at['Duration']
+        small_slot_i = int(np.argmin(slot_sums))
+        schedule[small_slot_i].append(surgery)
+
+        slot_sums[small_slot_i] += df_surg.iloc[surgery_id].at['Duration']
+
+    for i, slot in enumerate(schedule):
+        schedule[i] = np.array(slot)
+
+    skip_i = []
+
+    no = 0
+    while not all(slot > 480 for slot in slot_sums):
+        print("round:")
+        min_slot_i = int(np.argmin(slot_sums))
+        print("min slot: " + str(min_slot_i))
+
+        if not min_slot_i in skip_i:
+            skip_i.append(min_slot_i)
+
+        max_sum = 0
+        max_slot_i = -1
+        for i, slot_sum in enumerate(slot_sums):
+            if not i in skip_i:
+                if slot_sum > max_sum:
+                    max_sum = slot_sum
+                    max_slot_i = i
+        if max_slot_i == -1:
+            print("no max found")
+            break
+        print("max slot: " + str(max_slot_i))
+        over_slot = slot_sums[max_slot_i] - c
+        surgs_max_slot = schedule[max_slot_i]
+        small_surgery_i = int(np.argmin(surgs_max_slot[:, 1]))
+        small_surgery_id = surgs_max_slot[small_surgery_i, 0]
+        small_surgery_duration = df_surg.iloc[small_surgery_id].at['Duration']
+        if small_surgery_duration > over_slot:
+            skip_i.append(max_slot_i)
+            print("no small surgery, skipping: " + str(max_slot_i))
+        else:
+            skip_i = []
+            surgs_max_slot_list = surgs_max_slot.tolist()
+            surgs_max_slot_list.pop(small_surgery_i)
+            surgs_max_slot = np.array(surgs_max_slot_list)
+            schedule[max_slot_i] = surgs_max_slot
+            surgs_min_slot_list = schedule[min_slot_i].tolist()
+            surgs_min_slot_list.append([small_surgery_id, small_surgery_duration])
+            schedule[min_slot_i] = np.array(surgs_min_slot_list)
+
+            for i, slot in enumerate(schedule):
+                slot_sum = 0
+                for surgery in slot:
+                    slot_sum += surgery[1]
+                slot_sums[i] = slot_sum
+
+        no += 1
+        #if no > 1:
+        #    print(slot_sums)
 
     return schedule, slot_sums
 
 
 schedule, slot_sums = min_slot_assignment()
 
-print(c)
+print(slot_sums)
+print(schedule)
