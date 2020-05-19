10886641418101ddb22ccc46ad5a88794300ae07
Tip ten Brink <T.M.tenBrink@student.tudelft.nl>
2020-05-19 13:00:50 +0200

    Comments en toevoegingen, VeryLarge dataset
    VeryLarge dataset is veel groter (~1200 variabelen vs ~120 voor Large)
    Gurobi toegevoegd aan main program, als niet available wordt andere gebruikt
    Comments toegevoegd en print aangepast naar print naar file
    Output print in functie, andere naam, wordt nu ook geprint in model

--- model/ilp_model.py
+++ model/ilp_model.py
@@ -1,29 +1,40 @@
 from pulp import *
 import pandas as pd
+from model_result_print import *
 
+# List of importable datasets, each with a reference key so one can be chosen
 datasets = {
     'Small': "Data assignment operating room 2 Small.xlsx",
-    'Large': "Data assignment operating room 2 Large.xlsx"
+    'Large': "Data assignment operating room 2 Large.xlsx",
+    'VeryLarge': "Data assignment operating room 2 VeryLarge.xlsx"
 }
 
 print("Datasets to optimize: ")
 
+# Prints out the importable datasets and indicates their reference keys by looping through the keys
 for data_key in datasets.keys():
     print("Key: <" + str(data_key) + ">, Dataset name: '" + datasets[data_key] + "'")
 
+# Set to false to skip the dataset request phase and set to defaults below
 ask_dataset = True
+# Default key and file name
+filename = "Data assignment operating room 2 Large.xlsx"
+data_key_choice = 'Large'
 
+# A while loop so another key can be entered if a mistake is made
 while ask_dataset:
-    dataset_choice = input("\nType the exact key (what is in between '<>') of the desired dataset to begin optimizing.")
+    # The choice for the reference key listed above
+    data_key_choice = input(
+        "\nType the exact key (what is in between '<>') of the desired dataset to begin optimizing.\n")
+    # It looks for the inputted key in the datasets dictionary. If it exists, it is a correct key
+    # If the inputted key is incorrect, a KeyError will be thrown as none exists in the dictionary
+    # In the latter case it will restart the while loop to give another chance
     try:
-        filename = datasets[dataset_choice]
+        filename = datasets[data_key_choice]
         print("You have chosen the dataset: '" + filename + "'")
-        break
+        ask_dataset = False
     except KeyError:
         print("That is not a valid key Please try again.")
-
-if not ask_dataset:
-    filename = "Data assignment operating room 2 Large.xlsx"
 
 # Get pandas dataframe of the first sheet called 'General Information'
 # Index the dataframe by the names in the 0th column
@@ -45,50 +56,82 @@ c = df_par.loc['Capacity'].iat[0]
 # Get the amount of surgeries by counting the length of the index of the sheet (the 'Surgery' column)
 surgery_amount = len(df_surg.index)
 # Dictionary of surgery times
-surgeries = {} #mu
+surgeries_duration = {}  # mu
 for i in range(surgery_amount):
     # The surgeries are identified by numbers, so get the row by index (iloc)
     # This gets a whole row, then get the value at the 'Duration' column (at)
-    surgeries[i+1] = int(df_surg.iloc[i].at['Duration'])
+    surgeries_duration[i + 1] = int(df_surg.iloc[i].at['Duration'])
 
-I = range(1, surgery_amount+1)
-T = range(1, days_amount+1)
-R = range(1, rooms_amount+1)
+# Get the shape
+# I is the set of surgery numbers
+set_I = range(1, surgery_amount + 1)
+# T is the set of days
+set_T = range(1, days_amount + 1)
+# R is the set of rooms
+set_R = range(1, rooms_amount + 1)
 
-#make variables
-schedules = LpVariable.dicts("Schedule", (I, T, R), cat='Binary') #constraint 4
-max_overtimes = LpVariable.dicts("MaxOvertime", T, cat='Continuous') #m_t
-room_overtimes = LpVariable.dicts("RoomOvertime", (T, R), lowBound = 0, cat='Continuous') #m_r
+# Make 3 dictionaries for the problem variables
+# schedule_itr is the dictionary containing all the model x_ir variables, for r on a particular day t
+# This gives a 3-dimensional dictionary, where every individual value is either 1 or 0, satisfying C5
+# This indicates whether a surgery i is indeed scheduled in room r on day t (1 is scheduled, 0 not scheduled)
+schedule_itr = LpVariable.dicts("Schedule", (set_I, set_T, set_R), cat='Binary')
+# The overtimes are measured in minutes (integer)
+# max_overtimes is a dictionary containing all the model m_t variables, so the maximum overtime on a day t
+max_overtimes = LpVariable.dicts("MaxOvertime", set_T, cat='Integer')
+# room_overtimes is a dictionary containing all the m_r variables, so the overtime of a room r on a day t
+# As the overtime is zero if it is at less than capacity, we want a lower bound of 0, satisfying C3
+room_overtimes = LpVariable.dicts("RoomOvertime", (set_T, set_R), lowBound=0, cat='Integer')
 
-#initialize problem
+# Initialize the problem
+# We seek to minimize the overtime
 prob = LpProblem("OperatingRooms", LpMinimize)
 
-#objective function
-prob += lpSum([max_overtimes[t] for t in T])
+# Objective function is the sum of all maximum overtimes of the days, which we seek to minimize
+prob += lpSum([max_overtimes[t] for t in set_T])
 
-#constraints
-#constraint 1, 4
-for i in I:
-    prob += lpSum([schedules[i][t][r] for t in T for r in R]) == 1
+# Each surgery has to be scheduled exactly once, satisfying C1
+# So for a surgery i, all variables over all rooms r on all days t, must sum to exactly 1
+# This has to be the case for all surgeries, so we add this constraint for all surgeries
+for i in set_I:
+    prob += lpSum([schedule_itr[i][t][r] for t in set_T for r in set_R]) == 1
 
-#constraint 2
-for t in T:
-    for r in R:
-            prob += room_overtimes[t][r] - lpSum([surgeries[i]*schedules[i][t][r] for i in I]) + c == 0
-    
-#constraint 3    
-for t in T:
-    for r in R:
+# The overtime of a room r on a day t must be equal to the sum of the surgery times in that room minus the capacity
+# As a result, the difference must be equal to zero, satisfying C2
+# We only want the surgeries actually scheduled, so we multiply the binary schedule variable with the surgery duration
+# We want this for every room overtime variable m_r, so we loop through all r on all days t
+for t in set_T:
+    for r in set_R:
+        prob += room_overtimes[t][r] - lpSum([surgeries_duration[i] * schedule_itr[i][t][r] for i in set_I]) + c == 0
+
+# To be the maximum room overtime, m_t must be larger than all other m_r for the r on that day t, satisfying C4
+# As such, for all m_r, the corresponding m_t must be larger, requiring a constraint for all these
+for t in set_T:
+    for r in set_R:
         prob += max_overtimes[t] >= room_overtimes[t][r]
 
-prob.solve()
+# If the Gurobi solver is available, use it
+if GUROBI_CMD().available():
+    prob.solve(GUROBI_CMD())
+# Otherwise, solve it using the default solver
+else:
+    prob.solve()
+
+# Print whether the final status, indicating if it was indeed optimized
 print('Status', LpStatus[prob.status])
 
-for v in prob.variables():
-    print(v.name, '=', v.varValue)
-    
-print('Overtime sum =', value(prob.objective))
+# If it was indeed optimally solved, write the output to a text file for later viewing
+if LpStatus[prob.status] == 'Optimal':
+    # Define filename using the dataset key used in the choice at the start of this program
+    output_filename = "output_" + data_key_choice + ".txt"
+    # Open file, write mode so it is created if it does not exist
+    f = open(output_filename, 'w')
+    # Create list of lines containing all variable names and their values
+    lines = [str(v.name) + '=' + str(v.varValue) + "\n" for v in prob.variables()]
+    # Append objective function result
+    lines.append('Overtime sum =' + str(value(prob.objective)))
+    # Write lines to file
+    f.writelines(lines)
+    f.close()
 
-
-
-    
+    # Print result using function in model_result_print.py
+    model_result_print(output_filename)
---
+++ model/model_result_print.py
@@ -0,0 +1,119 @@
+# This program serves to create a nice printed overview of the results, based on the variables spitted out by the model
+# This allows the results to be viewed without running a full optimization
+# The program does not require the information from the dataset, so it can be run independently of the dataset
+
+
+def model_result_print(filename):
+    # The file is opened as the variable f
+    f = open(filename, "r")
+
+    # A list of lines of the text file is created
+    lines = f.readlines()
+
+    # It creates dictionaries corresponding to the model variable dictionaries
+    max_overtimes = {}
+    room_overtimes = {}
+    schedule = {}
+
+    # It reads line per line
+    for line in lines:
+        # If the line is empty (it strips it of whitespace), skip it
+        if not line.strip() == "":
+            # It creates a list of the line separated by the '='
+            equal_split = line.split('=')
+            # It splits the part before the '=', separated by underscores
+            underscore_split = equal_split[0].split('_')
+            # It strips the part after the '=' of whitespace, converts it into a float as it can contain decimals
+            # This is then converted to an int as only integer minutes are relevant in this problem
+            value = int(float(equal_split[1].strip()))
+
+            # The variable name is always before the underscore, so the 0th index of the underscore split
+            if underscore_split[0] == "MaxOvertime":
+                # If it is a MaxOvertime variable, it will put the value in the key corresponding to the day
+                # This day is, in the output format, the numerical value after the underscore
+                max_overtimes[int(underscore_split[1])] = value
+            elif underscore_split[0] == "RoomOvertime":
+                # If it is a RoomTime variable, the day is the value after the first underscore
+                day = int(underscore_split[1])
+                # The room is after the second underscore
+                room = int(underscore_split[2])
+
+                # Since shape is unknown beforehand, it must gradually create it
+                # It checks if there is already a key for the day added
+                try:
+                    v = room_overtimes[day]
+                # If there isn't, it will initialize this by setting an empty dictionary which will consist of the rooms
+                # It will also initialize schedule dictionary for that day, also corresponding to the rooms
+                except KeyError:
+                    room_overtimes[day] = {}
+                    schedule[day] = {}
+
+                # The value is the room overtime, so it sets it at the correct place
+                room_overtimes[day][room] = value
+
+                # It checks now also if a room has been added to the schedule on a certain day
+                try:
+                    v = schedule[day][room]
+                # If not, it creates an empty list where surgeries can be later added to
+                except KeyError:
+                    schedule[day][room] = []
+            elif underscore_split[0] == "Schedule":
+                # If the surgery is indeed scheduled in this room on this day, the value must be equal to 1
+                if value == 1:
+                    surgery = int(underscore_split[1])
+                    day = int(underscore_split[2])
+                    room = int(underscore_split[3])
+                    # It adds the surgery number to a list corresponding to a room on a day
+                    schedule[day][room].append(surgery)
+
+    # Print section
+    for day in room_overtimes.keys():
+        # For each day it prints an overview. The room_overtimes consists of keys corresponding to days
+        print("\nDAY " + str(day))
+        # For each room it prints an overview
+        for room in room_overtimes[day].keys():
+            # The values for room_overtime corresponding to day keys are dictionaries consisting of rooms with overtimes
+            room_overtime = room_overtimes[day][room]
+            # If there is overtime, the value will be greater than 0
+            if room_overtime > 0:
+                overdue_string = str(room_overtime) + " minutes overtime"
+            else:
+                overdue_string = "not overdue"
+            print("Room " + str(room) + ": " + overdue_string)
+
+            # It prints a list of surgeries in a room
+            room_surgeries = schedule[day][room]
+            for i, surgery in enumerate(room_surgeries):
+                # For the first surgery, it does not want to put a comma in front
+                if i == 0:
+                    sep = ""
+                else:
+                    sep = ", "
+                # It prints them on one line, separating using sep
+                print(sep + "Surgery " + str(surgery), end='')
+            print()
+
+    print("\n")
+
+    # Here it prints which rooms are the overdue ones on a certain day
+    for day in room_overtimes.keys():
+        max_overtime = max_overtimes[day]
+        # It makes a list of the rooms that have the maximum overtime associated with the day
+        max_overtime_rooms = []
+        for room in room_overtimes[day].keys():
+            if room_overtimes[day][room] == max_overtime:
+                max_overtime_rooms.append(room)
+        # If it is greater than 1, use 'are' instead of 'is'  and such
+        if len(max_overtime_rooms) > 1:
+            room_overtime_string = "Rooms "
+            for overtime_room in max_overtime_rooms:
+                room_overtime_string += str(overtime_room) + " "
+            room_overtime_string += "are "
+        else:
+            room_overtime_string = "Room " + str(max_overtime_rooms[0]) + " is "
+        print("On day " + str(day) + ", " + room_overtime_string + "most overdue, at " + str(max_overtime) +
+              " minutes")
+
+
+filename = "output_VeryLarge.txt"
+model_result_print(filename)
